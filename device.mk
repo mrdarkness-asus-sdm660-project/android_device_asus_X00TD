a7831aaf019a5ea3091cf5beb1ee590f77d5f5bf
DarknessKiller <23330152+DarknessKiller@users.noreply.github.com>
2021-03-24 11:05:20 +0800

    Add Android Go Configs
    * Improvement toward 3GB without effecting ram management on higher end models.

--- device.mk
+++ device.mk
@@ -27,5 +27,13 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     X00TDWifiOverlay
 
+# Android GO Configs
+PRODUCT_SYSTEM_SERVER_COMPILER_FILTER := speed-profile
+PRODUCT_ALWAYS_PREOPT_EXTRACTED_APK := true
+PRODUCT_USE_PROFILE_FOR_BOOT_IMAGE := true
+PRODUCT_DEX_PREOPT_BOOT_IMAGE_PROFILE_LOCATION := frameworks/base/config/boot-image-profile.txt
+PRODUCT_MINIMIZE_JAVA_DEBUG_INFO := true
+PRODUCT_ART_TARGET_INCLUDE_DEBUG_BUILD := false
+
 # Inherit the proprietary files
 $(call inherit-product, vendor/asus/X00TD/X00TD-vendor.mk)
